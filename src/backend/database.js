a7f27d0aebcd5a021c51d2314fb55a31465e7c41
Nico <akebra77@gmail.com>
2024-03-02 17:08:23 +0100

    add changes in db configuration

--- src/backend/database.js
+++ src/backend/database.js
@@ -10,7 +10,7 @@ const knex = require("knex")({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
-  pool: { min: 0, max: 7 },
+  pool: { min: 2, max: 10 },
 });
 
 // Check that the connection works
